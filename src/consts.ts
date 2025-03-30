3ba58289f13d3c9cd434db2762745c47ad0c5535
Nikki <nikkigalusha@gmail.com>
2025-03-30 11:32:19 -0700

    Fix broken slug

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "ADHD With Nikki",
+  NAME: "ADHD with Nikki",
   EMAIL: "adhdwithnikki@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
@@ -10,17 +10,17 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "ADHD With Nikki is a space for all to explore ADHD.",
+  DESCRIPTION: "ADHD with Nikki is a space for all to explore neurodivergence.",
 };
 
-export const BLOG: Metadata = {
+export const POSTS: Metadata = {
   TITLE: "Posts",
-  DESCRIPTION: "A collection of writings about ADHD--personal experiences, research, management strategies, and more.",
+  DESCRIPTION: "A collection of personal experiences, research, management strategies, art, and more.",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Resources",
-  DESCRIPTION: "A collection of resources meant to support those with ADHD.",
+  DESCRIPTION: "A collection of resources meant to support those with ADHD or supporting people with ADHD.",
 };
 
 export const SOCIALS: Socials = [
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,12 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
+const posts = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
     draft: z.boolean().optional()
-  }),
-});
-
-const work = defineCollection({
-  type: "content",
-  schema: z.object({
-    company: z.string(),
-    role: z.string(),
-    dateStart: z.coerce.date(),
-    dateEnd: z.union([z.coerce.date(), z.string()]),
   }),
 });
 
@@ -32,4 +22,4 @@ const projects = defineCollection({
   }),
 });
 
-export const collections = { blog, work, projects };
+export const collections = { posts, projects };
